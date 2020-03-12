5470675ce9b7ec4b32aef374c1903cbaee2b280a
Aldmi1988 <aldmi@bk.ru>
2020-03-13 01:07:34 +0700

    Add router lib

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import "./App.css";
+import DeviceOption from "./components/DeviceOption"
+import About from "./components/About"
+import Header from "./components/Header"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Header />
+          <main>
+            <Switch>
+              <Route path="/About" component={About} />
+              <Route path="/" component={DeviceOption} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
     </div>
   );
 }
 
-export default App;
+
+
