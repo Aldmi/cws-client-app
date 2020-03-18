94b2837f6ac1e1df5111dfd2a9386dbccaa1a2b4
Aldmi1988 <aldmi@bk.ru>
2020-03-18 20:13:15 +0700

    Add Nav panel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Suspense, lazy } from "react";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav";
 
-const DeviceOption = lazy(() => import("./components/deviceOptionComponents/DeviceOption"));
+const DeviceOption = lazy(() =>  import("./components/deviceOptionComponents/DeviceOption"));
 const About = lazy(() => import("./components/About"));
 
 export default function App() {
   return (
     <div className="App">
-     <CssBaseline />
-      <Nav/>
+      <CssBaseline />
       <Router>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route exact path="/" component={DeviceOption} />
-            <Route path="/about" component={About} />
-          </Switch>
-        </Suspense>
+        <React.Fragment>
+          <Nav/>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route exact path="/" component={DeviceOption} />
+              <Route path="/about" component={About} />
+              <Route children={() => <h2>Not Found</h2>} />
+            </Switch>
+          </Suspense>
+        </React.Fragment>
       </Router>
     </div>
   );
